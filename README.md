0def3af511d2ce9d2e66b1d62fc688da0f01e99a
venkataratnamb20 <venkat4ratnam@gmail.com>
2021-12-12 04:54:07 +0530

    Added User model, profile manager and migrations to database

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rest API- Profiles
+[Django- model fields](https://docs.djangoproject.com/en/1.11/ref/models/fields/)
+
+[Django- custom model fields](https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#auth-custom-user)
+[Django- London App developer- models git](https://gist.github.com/LondonAppDev/01feb984211e1237f81ca3e4bdb1eeb7#file-byob-user-profile-model-py)
--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Help Django  work with our custom user model"""
+    def create_user(self, email, firstname, lastname, password=None):
+        """Create new user profile object"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, firstname=firstname, lastname=lastname)
+
+        user.set_password = password       # Encrypts the password.
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, firstname, lastname, password):
+        """Create new super user for a given details"""
+        user = self.create_user(email, firstname, lastname, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+
+class UserProfile(AbstractBaseUser, PendingDeprecationWarning):
+    """User profile in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstname']
+
+    def get_full_name(self):
+        """Used to get the user full name"""
+        return self.firstname + ' ' + self.lastname
+
+    def get_short_name(self):
+        """Used to get User short name"""
+        return self.firstname
+
+    def __str__(self):
+        """Django uses this when it needs to convert an object to a string"""
+        return self.email
+
