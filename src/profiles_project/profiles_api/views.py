35450b020c4fe14ab7528a28eeec39ef662bc2e4
venkataratnamb2 <venkat4ratnam@gmail.com>
2021-12-20 12:12:06 +0530

    permissions classes added

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-
+from rest_framework.authentication import TokenAuthentication
 from . import serializers
 from . import models
+from . import permissions
+
+
 # Create your views here.
 class HelloApiView(APIView):
     """Test API View"""
@@ -91,3 +94,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles Creating and updating profiles """
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
