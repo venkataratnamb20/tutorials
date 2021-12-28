a81fc643c6d9df9af42d1c35c1c6ec7356609c00
venkataratnamb2 <venkat4ratnam@gmail.com>
2021-12-28 11:01:42 +0530

    added README.md and .gitignore

---
+++ john-smilga/01-nodejs/app.js
@@ -0,0 +1,14 @@
+// node- npm
+// npm --version
+// install package locally to use in a specific project
+// npm i <package name>
+// install package globally to use it in any project
+// npm install -g <package name>
+// npm init (Initialize the project and create the package.json with step by step, enter to skip)
+// npm init -y (Intialize the project and create package.json with default settings)
+// npm install (Execute this command to install dependencies listed in package.json)
+// install npm dev dependencies
+// npm i <package name> --save-dev
+// npm i nodemon --save-dev
+
+console.log("Hello Venkat!!")
---
+++ john-smilga/README.md
@@ -0,0 +1,21 @@
+# Node and Exprress course
+
+## Topics
+
+- variables
+- first nodejs codes
+- builtin Functions
+- globals
+- Modules
+- Event Loop
+- Async Pattterns
+- Event Emmitter and Streams
+- Others
+
+## Course
+
+[Youtube- John Smilga- node-express course](https://youtu.be/Oe421EPjeBE)
+
+## GitHub
+
+https://github.com/john-smilga/node-express-course
